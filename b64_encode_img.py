1429568d490d01376a41c92b3b208cb92b9bcf60
offsecj <offsecj@protonmail.com>
2024-06-28 16:14:45 +0300

    Image base64 encoder code added

--- b64_encode_img.py
+++ b64_encode_img.py
@@ -1,7 +1,7 @@
 import base64
 
 # Open the image file in binary read mode
-with open('C:\\Users\\j1024z\\Desktop\\vtt_rt\\phish\\vtt_sig_logo.png', 'rb') as image_file:
+with open('C:\\Users\\user\\Desktop\\logo.png', 'rb') as image_file:
     # Read the image file as binary data
     image_binary = image_file.read()
     # Encode the binary data as base64
