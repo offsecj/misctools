7f443edc088cdff8b476a670544827415bc6a147
offsecj <offsecj@protonmail.com>
2024-11-19 15:03:36 +0200

    subdomain resolver PS added

---
+++ resolve_subdomains.ps1
@@ -0,0 +1,17 @@
+﻿# Define the input file path
+$SubdomainsFile = "M:\misc\subdomains.txt"
+
+# Read the file line by line and resolve each subdomain
+Get-Content $SubdomainsFile | ForEach-Object {
+    $Subdomain = $_.Trim() # Trim any extra spaces or newlines
+    try {
+        $IP = [System.Net.Dns]::GetHostAddresses($Subdomain).IPAddressToString | Select-Object -First 1
+        if ($IP) {
+            Write-Output "$Subdomain - $IP"
+        } else {
+            #Write-Output "$Subdomain - No IP Found"
+        }
+    } catch {
+        #Write-Output "$Subdomain - Error Resolving"
+    }
+}
